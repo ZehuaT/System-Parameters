0eab1d41dc9fe47309a289418f40bdadbc2ea0d5
ZehuaT <zehua.t815@gmail.com>
2025-10-27 09:34:21 +0000

    Update System-Parameter.m

--- System-Parameter.m
+++ System-Parameter.m
@@ -7,55 +7,55 @@ GridFrequency = 50; % unit: Hz
 GridRadFreq   = 2*GridFrequency*3.14159; % unit: rad/s, note: w
 GridVoltage   = 400; % unit: V
 wLg           = 0.2; % unit: p.u., note: SCR = 5 by default
-Rg            = 0.04; % unit: p.u.
+Rg            = 0.02; % unit: p.u.
 
 %% Filter Parameters
-wLf = 0.25; % unit: p.u.
-Rf  = 0.05; % unit: p.u.
+wLf = 0.05; % unit: p.u.
+Rf  = 0.005; % unit: p.u.
 Cf  = 0.02; % unit: p.u.
 
 %% Basic Converter Parameters
-SamplingFrequency  = 10; % unit: kHz
-SwitchingFrequency = 10; % unit: kHz
+SamplingFrequency  = 10e3; % unit: Hz
+SwitchingFrequency = 10e3; % unit: Hz
 dcLinkVoltage      = 800; % unit: V
 
-V_Reference        = 1; % unit: p.u.
-P_Reference        = 0.5; % unit: p.u.
-Q_Reference        = 0; % unit: p.u.
+VReference         = 1; % unit: p.u.
+PReference         = 0.5; % unit: p.u.
+QReference         = 0; % unit: p.u.
 tauReactivePower   = 0.03; % unit: sec
 
 KpCC               = 1; % unit: p.u.
-KiCC               = 282.7; % unit: p.u./s
+KiCC               = 1571; % unit: p.u./s
 kVF                = 1; % unit: p.u.
 tauVF              = 0.01; % unit: sec
 
 %% Basic GFM Parameters
 J    = 3.14; % unit: p.u./(rad/s^2)
 D    = 100; % unit: p.u./(rad/s)
-KpVC = 9; % unit: p.u.
-KiVC = 1.13; % unit: p.u./s
+KpVC = 4.8; % unit: p.u.
+KiVC = 25; % unit: p.u./s
 
 %% Basic GFL Parameters
-KpPLL  = 31.4; % unit: rad/s
+KpPLL  = 44.4; % unit: rad/s
 KiPLL  = 246.7; % unit: rad/(s^2)
 tauPLL = 5.3e-4; % unit: sec
-KpPC   = 0.06; % unit: p.u.
-KiPC   = 0.49; % unit: p.u./s
+KpPC   = 0.126; % unit: p.u.
+KiPC   = 19.7; % unit: p.u./s
 
 %% GFM-PV
 % null
 
 %% GFM-PQ
-KpQC = 0.06; % unit: p.u.
-KiQC = 0.49; % unit: p.u./s
+KpQC = 0.126; % unit: p.u.
+KiQC = 19.7; % unit: p.u./s
 
 %% GFL-PV
-KpQV = 0.06; % unit: p.u.
-KiQV = 0.49; % unit: p.u./s
+KpQV = 0.126; % unit: p.u.
+KiQV = 19.7; % unit: p.u./s
 
 %% GFL-PQ
-KpQV = 0.06; % unit: p.u.
-KiQV = 0.49; % unit: p.u./s
+KpQV = 0.126; % unit: p.u.
+KiQV = 19.7; % unit: p.u./s
 
 %% MC-GFM
 KpVdc           = 100.5; % unit: rad/s
@@ -84,3 +84,20 @@ kQC = 0; % unit: p.u.
 wPDroop = 0.01; % unit: p.u./(rad/s)
 KpQV = 0.06; % unit: p.u.
 KiQV = 0.49; % unit: p.u./s
+
+%% CHIL Experiments
+% Case A
+PReference = 0.5; % unit: p.u.
+Vreference = 1; % unit: p.u.
+
+% Case B
+Preference = 0.5; % unit: p.u.
+Qreference = 0.0247; % unit: p.u.
+
+% Case C
+% for GFL-PQ
+Preference = 0.5; % unit: p.u.
+Qreference = 0; % unit: p.u.
+% for PV-mode converters
+Preference = 0; % unit: p.u.
+Vreference = 1; % unit: p.u.
